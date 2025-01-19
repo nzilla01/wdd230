74c2d6c119ff5b30ff6a6c07a0c60b3f963fdd79
nzilla <Victorbliss044@gmail.com>
2025-01-19 15:04:35 +0100

    commit

---
+++ chamber/script/main.js
@@ -0,0 +1,13 @@
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = `Last Modification: ${lastModified}`;
+
+const name = "Okon Nsikak";
+const country = "Nigeria";
+const state = "Akwa Ibom State";
+const currentYear = new Date().getFullYear();
+
+// Create the full copyright string
+const copyrightText = `&copy; ${currentYear} 🌸 ${name} 🌸 ${country}, ${state}`;
+
+// Insert the copyright text into the element with id 'copyrightInfo'
+document.getElementById("copyrightInfo").innerHTML = copyrightText;
